f063ca60c947e46b279150a4475d3ea890af364b
EnzoMacedo11 <enzomacedo15@gmail.com>
2022-12-19 14:23:46 -0300

    feat: criação do controller register

---
+++ src/routers/registerRouters.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { signIn,signUp } from "../controllers/register.controllers.js";
+import {validSchemaRegister} from "../middlewares/register.middleware.js"
+
+const router = Router();
+
+router.post("/signup", validSchemaRegister, signUp)
+
+router.post("signin", signIn)
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express  from "express";
 import cors from "cors";
 import dotenv from "dotenv"
+import registerRouters from "./routers/registerRouters.js"
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(registerRouters)
 
 
 const port = process.env.PORT || 4000
