a154bd3007f91b26d9098932e7c8fbee355d5ceb
EnzoMacedo11 <enzomacedo15@gmail.com>
2022-12-19 22:10:07 -0300

    feat: finalização do terceiro post

--- src/routers/urlsRouters.js
+++ src/routers/urlsRouters.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { shorten,urlId,openShort,deleteId } from "../controllers/urls.controllers.js";
+import getToken from "../middlewares/authToken.middleware.js";
 import {validSchemaRegister} from "../middlewares/shorten.middleware.js"
 
 const router = Router();
 
-router.post("/urls/shorten", validSchemaRegister, shorten)
+router.post("/urls/shorten", validSchemaRegister, getToken, shorten)
 
 router.get("/urls/:id", urlId)
 
